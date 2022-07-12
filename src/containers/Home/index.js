d0072516788a939afd3f5394997a17b72267b2cb
chamchiking <chamj61047@gmail.com>
2022-07-12 19:16:41 +0900

    새로 업로드

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -4,9 +4,9 @@ import "react-vertical-timeline-component/style.min.css";
 import Timeline from "./components/Timeline";
 import AnimatedText from "react-animated-text-content";
 import Container from "../../components/Container";
-import { Image } from "semantic-ui-react";
-import VoxelDog from '../../components/voxel-dog';
-
+import { Image} from "semantic-ui-react";
+import { Box } from '@chakra-ui/react'
+import VoxelDog from "../../components/voxel-dog";
 
 export default function Home() {
   const src = "mincheol.png";
@@ -15,25 +15,27 @@ export default function Home() {
       <br />
       <br />
       <Container>
-        {/* <VoxelDog /> */}
-      </Container>
-      <Container>
-          <Image src={src} size="small" floated="left" rounded />
-          <AnimatedText
-            type="chars"
-            animationType="bounce"
-            interval={0.06}
-            duration={0.8}
-            tag="h3"
-            includeWhiteSpaces
-            rootMargin="20%"
-          >
-            Mincheol Cha
-          </AnimatedText>
-          <p>
-            I am an undergraduate student at Seoul National University, studying
-            ECE.
-          </p>
+        <VoxelDog />
+        <br/><br/><br/><br/>
+        <Box borderRadius="lg" bg="greenyellow" p={3} align="center">
+          Hello I'm a Full Stack Developer
+        </Box>
+        <AnimatedText
+          type="chars"
+          animationType="bounce"
+          interval={0.06}
+          duration={0.8}
+          tag="h3"
+          includeWhiteSpaces
+          rootMargin="20%"
+        >
+          Mincheol Cha
+        </AnimatedText>
+        <Image src={src} size="small" floated="left" rounded />
+        <p>
+          I am an undergraduate student at Seoul National University, studying
+          ECE.
+        </p>
       </Container>
       <Container>
         <h4>Featured publications</h4>
